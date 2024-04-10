6d4adfc8012c9f7a6945047a31b5d119d3845262
vishal-github-org <vishukhape@gmail.com>
2024-04-10 18:17:24 +0530

    build folder created successfully

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+const nextConfig = {
+  // Add this configuration
+  // Enable static HTML export
+  output: "export",
+};
+
+module.exports = nextConfig;
